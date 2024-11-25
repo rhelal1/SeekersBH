756ea9ab6ec1c2b4c77050afb0381c2a9370541b
rhelal1 <ruqayahhelal@gmail.com>
2024-11-25 12:39:56 +0300

    first commit

---
+++ SeekersBH/SeekersBH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeekersBH
+//
+//  Created by BP-36-201-19 on 25/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
