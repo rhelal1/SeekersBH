f878fea87bbb817a423e9086009365fb8d9b78c2
Marwa <marwa.alkhuzaei@gmail.com>
2024-12-11 22:11:05 +0300

    screen views marwa

--- SeekersBH/SeekersBH/controllers/ViewController.swift
+++ SeekersBH/SeekersBH/controllers/ViewController.swift
@@ -4,7 +4,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Marwa")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let myStoryBoard = UIStoryboard(name: "marwa", bundle: nil)
+
+        let myVC = myStoryBoard.instantiateInitialViewController()!
+        myVC.modalPresentationStyle = .fullScreen
+
+        present(myVC, animated: true)
     }
 
 }
-
