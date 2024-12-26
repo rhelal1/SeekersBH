43b9357c8721fc0f35b05c5b671facbceb11a4e4
ZainabMadann <zainabmadann@gmail.com>
2024-12-26 18:56:27 +0300

    idk

---
+++ SeekersBH/SeekersBH/controllers/MangeWebinarsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MangeWebinarsTableViewCell.swift
+//  SeekersBH
+//
+//  Created by Zainab Madan on 26/12/2024.
+//
+
+import UIKit
+
+
+class MangeWebinarsTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+   
+    
+    func update(with webinar: Webinar) {
+        titleLabel.text = webinar.title
+        descriptionLabel.text = webinar.description
+    }
+}
